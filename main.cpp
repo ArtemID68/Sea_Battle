03753668e5c6223201a8b4bbbb1dea6ffec28081
Artem Sokolov <207891139+ArtemID68@users.noreply.github.com>
2025-06-16 15:12:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+// Ход компьютера
+        cout << "\nХод противника:" << endl;
+        int compRow, compCol;
+        // Компьютер выбирает случайные координаты, при условии, что по ним ещё не стреляли
+        do {
+            compRow = rand() % BOARD_SIZE;
+            compCol = rand() % BOARD_SIZE;
+        } while (playerBoard[compRow][compCol] == 'X' || playerBoard[compRow][compCol] == '*');
 
+        cout << "Компьютер выбрал: " << compRow << " " << compCol << endl;
+        
+        if (playerBoard[compRow][compCol] == 'S') {
+            cout << "Компьютер попал в ваш корабль!" << endl;
+            playerBoard[compRow][compCol] = 'X';
+        } else {
+            cout << "Компьютер промахнулся." << endl;
+            playerBoard[compRow][compCol] = '*';
+        }
+
+        // Проверяем, остались ли ещё корабли у игрока
+        if (!shipsRemaining(playerBoard)) {
+            cout << "\nВсе ваши корабли потоплены. Вы проиграли." << endl;
+            break;
+        }
+
+        cout << "\nВаше поле:" << endl;
+        printBoard(playerBoard, false);
+    }
+
+    return 0;
+}
